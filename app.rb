2a2dcafbef1a5ab974513ff1434c9d5e159f2ad4
Rebecca <graham.cali@gmail.com>
2020-12-25 13:20:27 -0600

    commit tree 69c3c8645e92d1d3dfb9dab2668b4009c2bb5ac3

--- app.rb
+++ app.rb
@@ -3,4 +3,57 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get '/reversename/:name' do
+    @name = params[:name].reverse
+    "#{@name}"
+  end
+
+  get '/square/:number' do
+    @number = params[:number].to_i ** 2
+   "#{@number}"
+  end
+
+  get '/say/:number/:phrase' do
+    answer = ''
+
+    params[:number].to_i.times do 
+      answer += params[:phrase]
+    end
+    
+    answer
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+  get '/:operation/:number1/:number2' do
+
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+
+    answer = "Unable to perform this operation"
+
+    case params[:operation]
+    when 'add'
+      answer = (number1 + number2).to_s
+    when 'subtract'
+      answer = (number1 - number2).to_s
+    when 'multiply'
+      answer = (number1 * number2).to_s
+    when 'divide'
+      answer = (number1 / number2).to_s
+    end
+  
+  end
+
+
 end
+
+
+
+#Create a dynamic route at get '/say/:number/:phrase' that accepts a number and a phrase and returns that phrase in a single string the number of times given.
+
+#Create a dynamic route at get '/say/:word1/:word2/:word3/:word4/:word5' that accepts five words and returns a string containing all five words (i.e. word1 word2 word3 word4 word5).
+
+#Create a dynamic route at get '/:operation/:number1/:number2' that accepts an operation (add, subtract, multiply or divide) and performs the operation on the two numbers provided, returning a String. For example, going to /add/1/2 should render 3 as a String.
